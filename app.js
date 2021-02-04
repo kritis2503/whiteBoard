48d5124361c58fe4595aeea25ccfa971ad51ba03
krits2503 <khandelwalkriti9925@gmail.com>
2021-02-04 21:50:24 +0530

    update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ io.on('connection',function(socket){
 
 });
 
-let port=3000;
+let port=process.env.PORT || 3000;
 http.listen(port,function(){
     console.log("Server started at 3000!!!!!!!!!!!");
 })
